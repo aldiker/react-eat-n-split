9a7aed724ffbda45cb232f95a48e0ff432e7c7aa
aldiker <aldiker@ukr.net>
2024-01-25 16:34:47 +0200

    Added key prop to FormSplitBill comp

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ export default function App() {
 
             {selectedFriend && (
                 <FormSplitBill
+                    key={selectedFriend.id}
                     selectedFriend={selectedFriend}
                     onSplitBill={handleSplitBill}
                 />
